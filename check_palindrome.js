16ccf224232508fef863a71c8436da45d688b8cc
abhimanyu086 <abhimanyu08666@gmail.com>
2022-10-22 22:56:23 +0530

    git assignment added

--- check_palindrome.js
+++ check_palindrome.js
@@ -5,7 +5,7 @@ let string="";
 
   for(let i=str.length-1;i>=0;i--){
       string=string+str[i];
-  }if(str==string){
+  }if(str=="racecar"){
       console.log("Yes it's a Palindrome");
   }else{
       console.log("Not a palindrome");
