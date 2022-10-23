d61ce2ce0a87fa2d967b35daaf1dcb506a7ee328
prajit96 <115496998+prajit96@users.noreply.github.com>
2022-10-23 19:10:22 +0530

    Update check_palindrome.js

--- check_palindrome.js
+++ check_palindrome.js
@@ -1,11 +1,11 @@
 // check Palindrome or not
 
-let str=racecar;
+let str="racecar";
 let string="";
 
   for(let i=str.length-1;i>=0;i--){
       string=string+str[i];
-  }if(str=="racecar"){
+  }if(str==string){
       console.log("Yes it's a Palindrome");
   }else{
       console.log("Not a palindrome");
